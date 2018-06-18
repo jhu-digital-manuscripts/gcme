04f74572c7d092669bb6b18166ec023eacacb4d4
Mark Patton <mark.patton@gmail.com>
2018-06-18 07:29:48 -0400

    Separate out scripts for generating data and updating indicies.

---
+++ deploy/gen_data.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+echo -e "\n\n** Generating data"
+
+cd ../gcme-tool && mvn clean package && java -jar target/*-shaded.jar ../data gen-data && cd -
+
+mv ../gcme-tool/*.ndjson .
+
+mv ../gcme-tool/text-powersel.json ../gcme-ember/public
