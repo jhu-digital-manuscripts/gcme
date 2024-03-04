a360c9fe33fb90401893c69e4c5ea620002b2a30
Mark Patton <mark.patton@gmail.com>
2024-03-04 09:18:41 -0500

    Update to Java 17

--- deploy/gen_data.sh
+++ deploy/gen_data.sh
@@ -3,7 +3,7 @@
 echo -e "\n\n** Generating data"
 
 export GCME_DATA=`pwd`/../data
-cd ../gcme-tool && mvn clean package && java -jar target/*-shaded.jar ../data gen-data && cd -
+cd ../gcme-tool && mvn clean package && java -jar target/gcme-tool-*.jar ../data gen-data && cd -
 
 mv ../gcme-tool/*.ndjson .
 mv ../gcme-tool/*.json ../gcme-ember/public/
