9671e89bf48ca815bf438e39256a3fcf399fea04
Mark Patton <mark.patton@gmail.com>
2026-07-15 07:15:53 -0400

    Fix lint errors in gcme-ember

--- gcme-ember/app/routes/tags.js
+++ gcme-ember/app/routes/tags.js
@@ -1,20 +1,24 @@
 import Route from '@ember/routing/route';
-import { action } from '@ember/object';
 import fetchJson from 'gcme-ember/utils/fetch-json';
 
 export default class TagsRoute extends Route {
   async model() {
-    const rows = await fetchJson('/tag-table.json');
-    return { rows };
+    try {
+      const rows = await fetchJson('/tag-table.json');
+      return { rows, loadError: null };
+    } catch (err) {
+      return {
+        rows: [],
+        loadError:
+          err && err.message
+            ? err.message
+            : 'Could not load tag reference table.',
+      };
+    }
   }
 
-  @action
-  error(err) {
-    const controller = this.controllerFor('tags');
-    controller.loadError =
-      err && err.message
-        ? err.message
-        : 'Could not load tag reference table.';
-    return false;
+  setupController(controller, model) {
+    super.setupController(controller, model);
+    controller.loadError = model.loadError;
   }
 }
