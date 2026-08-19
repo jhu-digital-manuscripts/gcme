312e146e2e3c9724a8b755623a173301c54e5e71
Mark Patton <mark.patton@gmail.com>
2026-08-19 14:20:15 -0400

    Add GitHub actions for CI/CD and release. Support Opensearch and a custom local search instead of elasticsearch.

--- deploy/gen_data.sh
+++ deploy/gen_data.sh
@@ -1,10 +1,29 @@
 #! /bin/sh
+#
+# Generate the data the application is deployed with.
+#
+# The OpenSearch bulk ingest requests (*.ndjson) are written to this directory and the static data
+# of the Ember UI (*.json) to gcme-ember/public.
 
-echo -e "\n\n** Generating data"
+set -e
 
-export GCME_DATA=`pwd`/../data
-cd ../gcme-tool && mvn clean package && java -jar target/gcme-tool-*.jar ../data gen-data && cd -
+DEPLOY_DIR=$(pwd)
+DATA_DIR="$DEPLOY_DIR/../data"
+TOOL_DIR="$DEPLOY_DIR/../gcme-tool"
+PUBLIC_DIR="$DEPLOY_DIR/../gcme-ember/public"
+OUT_DIR=$(mktemp -d)
 
-mv ../gcme-tool/*.ndjson .
-mv ../gcme-tool/*.json ../gcme-ember/public/
+trap 'rm -rf "$OUT_DIR"' EXIT
 
+printf '\n\n** Building gcme-tool\n'
+
+(cd "$TOOL_DIR" && mvn -B clean package -DskipTests)
+
+printf '\n\n** Generating data\n'
+
+java -jar "$TOOL_DIR/target/gcme-tool.jar" gen-data "$DATA_DIR" --output "$OUT_DIR"
+
+mv "$OUT_DIR"/*.ndjson "$DEPLOY_DIR/"
+mv "$OUT_DIR"/*.json "$PUBLIC_DIR/"
+
+printf '\n\n** Wrote ndjson files to %s and json files to %s\n' "$DEPLOY_DIR" "$PUBLIC_DIR"
--- deploy/update_indices.sh
+++ deploy/update_indices.sh
@@ -1,6 +1,6 @@
-#! /bin/sh
+#!/bin/sh
 
-echo "** Delete existing indicies"
+echo "** Delete existing indices"
 
 curl -X DELETE http://localhost:9200/line/
 curl -X DELETE http://localhost:9200/word_dict/
@@ -9,7 +9,7 @@ curl -X DELETE http://localhost:9200/lemma_tag_dict/
 
 sleep 5
 
-echo -e "\n\n** Create indicies"
+echo -e "\n\n** Create indices"
 
 curl -X PUT -H 'Content-Type: application/json; charset=UTF-8' http://localhost:9200/line/ --data-binary @line_index.json
 curl -X PUT -H 'Content-Type: application/json; charset=UTF-8' http://localhost:9200/word_dict/ --data-binary @word_dict_index.json
@@ -19,19 +19,16 @@ curl -X PUT -H 'Content-Type: application/json; charset=UTF-8' http://localhost:
 sleep 5
 echo -e "\n\n** Indexing data"
 
-curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/line/_doc/_bulk --data-binary "@line.ndjson"
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/line/_bulk --data-binary "@line.ndjson"
 
 sleep 5
 
-curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/word_dict/_doc/_bulk --data-binary "@word_dict.ndjson"
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/word_dict/_bulk --data-binary "@word_dict.ndjson"
 
 sleep 5
 
-curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/lemma_dict/_doc/_bulk --data-binary "@lemma_dict.ndjson"
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/lemma_dict/_bulk --data-binary "@lemma_dict.ndjson"
 
 sleep 5
 
-curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/lemma_tag_dict/_doc/_bulk --data-binary "@lemma_tag_dict.ndjson"
-
-
-
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/lemma_tag_dict/_bulk --data-binary "@lemma_tag_dict.ndjson"
