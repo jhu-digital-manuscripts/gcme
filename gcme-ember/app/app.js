d3c1568c4c0aac452a9950c25a466052ce73b823
Mark Patton <mark.patton@gmail.com>
2026-09-09 14:16:54 -0400

    Enable deprecation warnings in dev mode and fix a deprecation message

--- gcme-ember/app/app.js
+++ gcme-ember/app/app.js
@@ -2,6 +2,13 @@ import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import { macroCondition, isDevelopingApp, importSync } from '@embroider/macros';
+
+// Only load the deprecation workflow while developing/testing. The macro
+// strips this branch (and the imported module) from production builds.
+if (macroCondition(isDevelopingApp())) {
+  importSync('./deprecation-workflow');
+}
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
